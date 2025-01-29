90d1a9d92c191eba5edb824647d505934cfb655a
Husk Stormy <88177050+HuskStormy@users.noreply.github.com>
2025-01-28 21:25:22 -0600

    #

---
+++ src/app/layout/component/app.menu.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { AppMenuitem } from './app.menuitem';
+
+@Component({
+    selector: 'app-menu',
+    standalone: true,
+    imports: [CommonModule, AppMenuitem, RouterModule],
+    template: `
+    <ul class="layout-menu">
+        <ng-container *ngFor="let item of model; let i = index">
+            <li app-menuitem *ngIf="!item.separator" [item]="item" [index]="i" [root]="true"></li>
+            <li *ngIf="item.separator" class="menu-separator"> </li>
+        </ng-container>
+    </ul> `
+})
+export class AppMenu {
+    model: MenuItem[] = [];
+
+    ngOnInit() {
+        this.model = [
+            {
+                label: 'Index',
+                items: [{ label: 'Dashboard', icon: 'pi pi-fw pi-home', routerLink: ['/'] }]
+            },
+            {
+                label: 'Modulo',
+                items: [
+                    
+                    {   label: 'Beca',
+                        icon: 'pi pi-fw pi-circle',                                
+                        items: [
+                        { label: 'Convocatoria',icon: 'pi pi-fw pi-bookmark', routerLink: ['/Prueba/convocatoria']},
+                        { label: 'Solicitud',   icon: 'pi pi-fw pi-bookmark', routerLink: ['/Prueba/solicitud'] },
+                        { label: 'Desembolso',  icon: 'pi pi-fw pi-bookmark'},
+                        { label: 'Cierre',      icon: 'pi pi-fw pi-bookmark'}
+                    ] }
+                ]
+            }
+        ];
+    }
+}
