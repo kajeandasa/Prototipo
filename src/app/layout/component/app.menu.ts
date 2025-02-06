d61cdaf20b851e3084558dbf7f02735cc97f68a3
KareliaAntunez <rivaskareliacecilia@gmail.com>
2025-02-06 12:23:16 -0600

    Cambios en el menu principal

--- src/app/layout/component/app.menu.ts
+++ src/app/layout/component/app.menu.ts
@@ -46,22 +46,41 @@ export class AppMenu {
                                 { label: 'Paso 2: Aval recibe la solicitud', icon: 'pi pi-check', routerLink: ['/beca/aprobacionaval'] }
                             ]
                         },
-                        { label: 'Solicitudes',   icon: 'pi pi-fw pi-file-import', routerLink: ['/beca/solicitud'] },
-                        { label: 'Desembolso',  icon: 'pi pi-fw pi-money-bill'},
-                        { label: 'Cierre',      icon: 'pi pi-fw pi-delete-left'}
                     ] },
-                    {   label: 'Aval',
-                        icon: 'pi pi-fw pi-circle',                                
-                        items: [
-                        { label: 'Convocatoria',icon: 'pi pi-fw pi-bookmark', routerLink: ['/beca/convocatoria']},
-                        { label: 'Solicitudes',   icon: 'pi pi-fw pi-file-import', routerLink: ['/beca/solicitud'] },
-                        { label: 'Desembolso',  icon: 'pi pi-fw pi-money-bill'},
-                        { label: 'Cierre',      icon: 'pi pi-fw pi-delete-left'}
-                    ] }
-
                 ]
             },
-
+            {
+                label: 'Mantenimiento',
+                items: [
+                    
+                    {   label: 'Desembolso',
+                        icon: 'pi pi-fw pi-circle',                                
+                        items: [
+                        { label: 'Becas',icon: 'pi pi-fw pi-book', 
+                            items: [
+                                { label: 'Tipos de Becas', icon: 'pi pi-address-book', routerLink: ['/beca/solicitudpostulante'] },
+                                { label: 'Desembolsos por beca', icon: 'pi pi-wallet', routerLink: ['/beca/aprobacionaval'] }
+                            ]
+                        },
+                        { label: 'Estados',   icon: 'pi pi-fw pi-lightbulb', 
+                            items: [
+                                { label: 'Estado de Prorroga', icon: 'pi pi-clock', routerLink: ['/beca/solicitudpostulante'] },
+                                { label: 'Estado de Seguimiento', icon: 'pi pi-folder-open', routerLink: ['/beca/aprobacionaval'] }
+                            ]    
+                        },
+                        { label: 'Prorroga',  icon: 'pi pi-fw pi-file-plus',
+                            items: [
+                                { label: 'Plazo', icon: 'pi pi-calendar-clock', routerLink: ['/beca/solicitudpostulante'] },
+                            ]
+                        },
+                        { label: 'Cierre',      icon: 'pi pi-fw pi-stopwatch',
+                            items: [
+                                { label: 'Tipo de Cierre', icon: 'pi pi-ellipsis-v', routerLink: ['/beca/solicitudpostulante'] },
+                            ]
+                        }
+                    ] },
+                ]
+            },
             
         ];
     }
