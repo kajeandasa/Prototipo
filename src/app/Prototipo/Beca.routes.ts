ba87392e1451633f3f0af10ede5c49bb5b6fa862
Husk Stormy <88177050+HuskStormy@users.noreply.github.com>
2025-02-11 09:28:01 -0600

    #

--- src/app/Prototipo/Beca.routes.ts
+++ src/app/Prototipo/Beca.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { ConvocatoriaComponent } from './convocatoria/convocatoria.component';
 import { SolicitudComponent } from './solicitud/solicitud.component';
+import { FormularioComponent } from './Formularios/formulario/formulario.component'
+import { ItemFormularioComponent } from './Formularios/item-formulario/item-formulario.component'
 
 
 export default [
     { path: 'convocatoria', data: { breadcrumb: 'convocatoria' }, component: ConvocatoriaComponent },
     { path: 'solicitud', data: { breadcrumb: 'solicitud' }, component: SolicitudComponent },
+    { path: 'formulario',    data: { breadcrumb: 'formulario' },     component: FormularioComponent },
+    { path: 'itemformulario',    data: { breadcrumb: 'itemformulario' },     component: ItemFormularioComponent },
+    { path: 'itemformulario/:id',    data: { breadcrumb: 'itemformulario' },     component: ItemFormularioComponent },
     { path: '**', redirectTo: '/notfound' }
 ] as Routes;
