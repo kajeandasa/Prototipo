b935ca19ea596e4e2909a2ae7e5917520e700b68
Husk Stormy <88177050+HuskStormy@users.noreply.github.com>
2025-02-12 13:14:43 -0600

    #formulario detalles

--- src/app/Prototipo/Formularios/formulario-usuario/formulario-usuario.component.ts
+++ src/app/Prototipo/Formularios/formulario-usuario/formulario-usuario.component.ts
@@ -79,15 +79,15 @@ export class FormularioUsuarioComponent {
     // datos de los formulario
     Formulario = [
       { id: 1, Titulo: 'Formulario planilla', Descripcion: 'descripcion al formulario', fechaCreacion: new Date('2012-12-12') },
-      { id: 2, Titulo: 'Formulario para ', Descripcion: 'descripcion al formulario', fechaCreacion: new Date('2012-12-12') }
+      { id: 2, Titulo: 'Formulario beca postulante', Descripcion: 'descripcion al formulario', fechaCreacion: new Date('2012-12-12') }
     ];
     //inputs
     input = [
         { id: 1, formulario: 1, Pregunta: 'pregunta para responer texto',     tipo: 1, fechaCreacion: new Date('2012-12-12') },
         { id: 2, formulario: 2, Pregunta: 'pregunta para responer numero',    tipo: 2, fechaCreacion: new Date('2012-12-12') },
         { id: 3, formulario: 1, Pregunta: 'pregunta para responer parrafo',   tipo: 3, fechaCreacion: new Date('2012-12-12') },
-        { id: 4, formulario: 2, Pregunta: 'pregunta para responer Cargar Archivo', tipo: 4, fechaCreacion: new Date('2012-12-12') },
-        { id: 5, formulario: 2, Pregunta: 'pregunta para responer unica',     tipo: 5, fechaCreacion: new Date('2012-12-12') },
+        { id: 4, formulario: 1, Pregunta: 'pregunta para responer Cargar Archivo', tipo: 4, fechaCreacion: new Date('2012-12-12') },
+        { id: 5, formulario: 1, Pregunta: 'pregunta para responer unica',     tipo: 5, fechaCreacion: new Date('2012-12-12') },
         { id: 6, formulario: 1, Pregunta: 'pregunta para responer multiple',  tipo: 6, fechaCreacion: new Date('2012-12-12') }
     ];
     // tipo de inputs
@@ -104,8 +104,9 @@ export class FormularioUsuarioComponent {
         {id:1, name: 'respuesta 1', key: 'A', id_input: 5},
         {id:2, name: 'respuesta 2', key: 'M', id_input: 6},
         {id:3, name: 'respuesta 3', key: 'P', id_input: 5},
-        {id:4, name: 'respuesta 4', key: 'R', id_input: 6},
+        {id:4, name: 'respuesta 4', key: 'R', id_input: 6}
     ];
+
 
 
     // Método para filtrar inputs dependiendo del formulario
