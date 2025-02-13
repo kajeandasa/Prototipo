7ba6ba2fb63d033098dbbb1ee94f7fc71a89cbc1
Husk Stormy <88177050+HuskStormy@users.noreply.github.com>
2025-02-13 08:51:38 -0600

    aqui

--- src/app/Prototipo/Beca.routes.ts
+++ src/app/Prototipo/Beca.routes.ts
@@ -5,7 +5,6 @@ import { FormularioComponent } from './Formularios/formulario/formulario.compone
 import { ItemFormularioComponent } from './Formularios/item-formulario/item-formulario.component'
 import { FormularioUsuarioComponent } from './Formularios/formulario-usuario/formulario-usuario.component';
 import { AlertaComponent } from './alertas/alerta/alerta.component'
-import { TipoAlertaComponent } from './alertas/tipo-alerta/tipo-alerta.component';
 
 export default [
     { path: 'convocatoria',         data: { breadcrumb: 'convocatoria' },   component: ConvocatoriaComponent },
@@ -14,6 +13,5 @@ export default [
     { path: 'formulario-usuario',   data: { breadcrumb: 'Usuarioformulario'},component: FormularioUsuarioComponent },
     { path: 'itemformulario/:id',   data: { breadcrumb: 'itemformulario' }, component: ItemFormularioComponent },
     { path: 'alerta',   data: { breadcrumb: 'alerta' }, component: AlertaComponent },
-    { path: 'alerta_tipo',   data: { breadcrumb: 'alerta_tipo' }, component: TipoAlertaComponent },
     { path: '**', redirectTo: '/notfound' }
 ] as Routes;
--- src/app/Prototipo/Manenimiento.routes.ts
+++ src/app/Prototipo/Manenimiento.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { FormularioManteminientoComponent } from './Formularios/formulario-Manenimiento/formularioMantenimiento.component';
+import { TipoAlertaComponent } from './alertas/tipo-alerta/tipo-alerta.component';
 
 export default [
     { path: 'formulario', data: { breadcrumb: 'formulario' }, component: FormularioManteminientoComponent },
+    { path: 'alerta_tipo',   data: { breadcrumb: 'alerta_tipo' }, component: TipoAlertaComponent },
     { path: '**', redirectTo: '/notfound' }
 ] as Routes;
