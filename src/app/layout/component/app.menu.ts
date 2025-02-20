3f6064673c3de595073d1b08529515e6a506adb9
KareliaAntunez <rivaskareliacecilia@gmail.com>
2025-02-20 12:13:58 -0600

    cambios de errores

--- src/app/layout/component/app.menu.ts
+++ src/app/layout/component/app.menu.ts
@@ -29,9 +29,6 @@ export class AppMenu {
                 label: 'Modulo',
                 items: [
 
-                    {   label: 'Beca',
-                        icon: 'pi pi-fw pi-circle',
-                        items: [
                         { label: 'Convocatoria',icon: 'pi pi-fw pi-bookmark', routerLink: ['/beca/convocatoria']},
                         { label: 'Becas',icon: 'pi pi-fw pi-bookmark', routerLink: ['/beca/infobeca']},
                         { label: 'Solicitudes',   icon: 'pi pi-fw pi-bookmark', routerLink: ['/beca/solicitud']},
@@ -41,7 +38,7 @@ export class AppMenu {
                         { label: 'Alerta',   icon: 'pi pi-fw pi-bookmark', routerLink: ['/beca/alerta'] },
                         { label: 'Formulario',   icon: 'pi pi-fw pi-bookmark', routerLink: ['/beca/formulario'] },
                         { label: 'Documentos',   icon: 'pi pi-fw pi-bookmark', routerLink: ['/beca/ItemDocumento'] },
-                    ] },
+                    
                     {   label: 'Becario',
                         icon: 'pi pi-fw pi-circle',
                         items: [
@@ -58,44 +55,12 @@ export class AppMenu {
                 label: 'Mantenimiento',
                 items: [
                     
-                    {   label: 'Desembolso',
-                        icon: 'pi pi-fw pi-circle',                                
-                        items: [
                         { label: 'Becas', icon: 'pi pi-fw pi-book', 
                             items: [
                                 { label: 'Desembolsos por beca', icon: 'pi pi-wallet', routerLink: ['/beca/desembolsosbeca'] },
                                 { label: 'Estado Desembolso Pagado', icon: 'pi pi-asterisk', routerLink: ['/beca/'] },
                                 { label: 'Tipos de Becas', icon: 'pi pi-address-book', routerLink: ['/beca/tipobeca'] },
                                 { label: 'Monto de la Beca', icon: 'pi pi-money-bill', routerLink: ['/beca/montodefaultbeca'] },
-                                { label: 'Formulario', icon: 'pi pi-fw pi-bookmark', routerLink: ['/mantenimiento/formulario'] },
-                                {
-                                    label: 'Alerta',
-                                    icon: 'pi pi-fw pi-bookmark',
-                                    items: [
-                                        { label: 'Tipo Alerta', routerLink: ['/mantenimiento/alerta_tipo'] }
-                                    ]
-                                },                            {
-                                    label: 'Documentos',
-                                    icon: 'pi pi-fw pi-bookmark',
-                                    items: [
-                                        { label: 'Requisitos', routerLink: ['/mantenimiento/plantilla'] }
-    
-    
-                                    ]
-                                },
-                                {
-                                    label: 'Convocatoria',
-                                    icon: 'pi pi-fw pi-bookmark',
-                                    items: [
-                                        { label: 'Estado convocatoria', routerLink: ['/mantenimiento/estado'] },
-                                        { label: 'Tipo convocatoria', routerLink: ['/mantenimiento/tipo'] }
-    
-    
-    
-                                    ]
-                                },
-                                { label: 'Desembolso', icon: 'pi pi-fw pi-bookmark' },
-                                { label: 'Cierre', icon: 'pi pi-fw pi-bookmark' }
                             ]
                         },
                         { label: 'Seguimiento',   icon: 'pi pi-fw pi-folder-open', 
@@ -114,8 +79,36 @@ export class AppMenu {
                             items: [
                                 { label: 'Tipo de Cierre', icon: 'pi pi-ellipsis-v', routerLink: ['/beca/tipocierre'] },
                             ]
-                        }
-                    ] },
+                        },
+                        { label: 'Formulario', icon: 'pi pi-fw pi-bookmark', routerLink: ['/mantenimiento/formulario'] },
+                        {
+                            label: 'Alerta',
+                            icon: 'pi pi-fw pi-bookmark',
+                            items: [
+                                { label: 'Tipo Alerta', routerLink: ['/mantenimiento/alerta_tipo'] }
+                            ]
+                        },                            
+                        {
+                            label: 'Documentos',
+                            icon: 'pi pi-fw pi-bookmark',
+                            items: [
+                                { label: 'Requisitos', routerLink: ['/mantenimiento/plantilla'] }
+
+
+                            ]
+                        },
+                        {
+                            label: 'Convocatoria',
+                            icon: 'pi pi-fw pi-bookmark',
+                            items: [
+                                { label: 'Estado convocatoria', routerLink: ['/mantenimiento/estado'] },
+                                { label: 'Tipo convocatoria', routerLink: ['/mantenimiento/tipo'] }
+
+
+
+                            ]
+                        },
+                    
                 ],
 
 
