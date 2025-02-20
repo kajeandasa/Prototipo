2d2c7165fd92646d420918d1c5a2948d981929f1
KareliaAntunez <rivaskareliacecilia@gmail.com>
2025-02-20 11:09:25 -0600

    Validaciones

--- src/app/Prototipo/desembolso/estandares/tablasmantenimiento.component.ts
+++ src/app/Prototipo/desembolso/estandares/tablasmantenimiento.component.ts
@@ -29,7 +29,6 @@ import { AccordionModule } from 'primeng/accordion';
 import { TabsModule } from 'primeng/tabs';
 import { FileUpload } from 'primeng/fileupload';
 import { ToggleSwitch } from 'primeng/toggleswitch';
-import { DropdownModule } from 'primeng/dropdown';
 
 // Interfaz para gestionar filas expandidas en tablas
 interface expandedRows {
@@ -37,7 +36,7 @@ interface expandedRows {
 }
 
 @Component({
-  selector: 'app-convocatoria',
+  selector: 'app-cierre',
   imports: [
     ListboxModule,
     CalendarModule,
@@ -63,58 +62,33 @@ interface expandedRows {
     KeyFilterModule,
     AccordionModule,
     TabsModule,
-    DropdownModule
   ],
-  templateUrl: './montodefaultbeca.component.html',  // Enlaza la plantilla HTML del componente
-  styleUrl: './montodefaultbeca.component.scss',   // Enlaza el archivo de estilos SCSS del componente
+  templateUrl: './tablasmantenimiento.component.html',  // Enlaza la plantilla HTML del componente
+  styleUrl: './tablasmantenimiento.component.scss',   // Enlaza el archivo de estilos SCSS del componente
   standalone: true,
   providers: [MessageService, ConfirmationService], // Asegurar que MessageService esté disponible
 })
-export class montodefaultbecaComponent {
+export class tablasmantenimiento {
 
-  // Array con los datos de la tabla del monto real de las becas
+  // Array con los datos de la tabla tipo de beca
   tasks = [
     {
         id: 1,
         nombre: 'Beca sustantiva de investigación e innovación en pro de impactos en la realidad nacional',
-        monto: '500,000.00',
-        duracion: '12 meses',
     },
     {
         id: 2,
         nombre: 'Beca básica institucional para el fortalecimiento de grupos de investigación',
-        monto: '75,000.00',
-        duracion: '12 meses',
     },
     {
         id: 3,
         nombre: 'Beca básica de grado como espacios de aprendizaje del método científico',
-        monto: '25,000.00',
-        duracion: '6 meses',
     },
     {
         id: 4,
         nombre: 'Beca básica de postgrado para la eficiencia terminal de tesis',
-        monto: '40,000.00',
-        duracion: '6 meses',
-    },
-    {
-        id: 5,
-        nombre: 'Beca básica de postgrado para la eficiencia terminal de tesis',
-        monto: '80,000.00',
-        duracion: '6 meses',
     },
 
-  ];
-
-  // Array para almacenar los becas para el select
-  selectedBeca: any[] = [];
-
-  beca = [
-      { name: 'Beca sustantiva de investigación e innovación en pro de impactos en la realidad nacional', code: 'BS' },
-      { name: 'Beca básica institucional para el fortalecimiento de grupos de investigación', code: 'BGI' },
-      { name: 'Beca básica de grado como espacios de aprendizaje del método científico', code: 'BG' },
-      { name: 'Beca básica de postgrado para la eficiencia terminal de tesis', code: 'BP' }
   ];
   
   // Variable para controlar el estado de carga
@@ -131,7 +105,8 @@ export class montodefaultbecaComponent {
   clear(table: Table, searchInput: HTMLInputElement) {
     table.clear(); // Limpia los filtros aplicados en la tabla
     searchInput.value = ''; // Borra el contenido del input de búsqueda
-  }  
+  }
+  
 
   // Filtro global para buscar en todas las columnas de la tabla
   onGlobalFilter(table: any, event: any) {
@@ -171,35 +146,5 @@ export class montodefaultbecaComponent {
   
   //Declara que el boton de switch esta en no o cancelar
   checked: boolean = false;
-
-  // Array para almacenar la cantidad para el select
-  selectedcantidad: any[] = [];
-
-  cantidad = [
-      { name: '1', code: '1' },
-      { name: '2', code: '2' },
-      { name: '3', code: '3' },
-      { name: '4', code: '4' },
-      { name: '5', code: '5' },
-      { name: '6', code: '6' },
-      { name: '7', code: '7' },
-      { name: '8', code: '8' },
-      { name: '9', code: '9' },
-      { name: '10', code: '10' },
-      { name: '11', code: '11' },
-      { name: '12', code: '12' },
-  ];
-
-  // Array para almacenar la unidad de tiempo para el select
-  selectedunidadtiempo: any[] = [];
-
-  unidadtiempo = [
-      { name: 'Mes', code: 'M' },
-      { name: 'Meses', code: 'MM' },
-      { name: 'Semestre', code: 'S' },
-      { name: 'Semestres', code: 'SS' },
-      { name: 'Año', code: 'A' },
-      { name: 'Años', code: 'AA' },
-  ];
     
 }
